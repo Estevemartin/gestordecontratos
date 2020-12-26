86c917483729385c65e0e6c0ab25c0addf3021d0
Esteve Martín <estevemartinmauri@hotmail.com>
2020-12-26 13:29:19 +0100

    KPIs Done

--- public/js/kpis.js
+++ public/js/kpis.js
@@ -2,6 +2,8 @@
 
 
 console.log("Printing KPIs")
+
+//Legend Margin
 Chart.Legend.prototype.afterFit = function() {
   this.height = this.height + 20;
 };
@@ -74,7 +76,7 @@ async function getContractsManagedInSixDaysData(chart,year){
     if (contract.mainStatus==="Closed"&&contractYearFilter===year){
       const monthNumber = Number(contract.fechaStatusWon.split('/')[1])
       let closingDays = getDaysBetweenDates(contract.fechaStatusWon,contract.historico[contract.historico.length-1].fecha)
-      // console.log(closingDays)
+      // console.log(contract.pq,closingDays)
       if(closingDays<=6){
         lessThanSixDays[monthNumber] = lessThanSixDays[monthNumber]+1
       } else {
@@ -98,10 +100,7 @@ async function getContractsManagedInSixDaysData(chart,year){
       return element
     }
   })
-  // console.log(lessThanSixDays)
-  // console.log(moreThanSixDays)
-
-  // console.log(chart)
+  
   chart.config.data.datasets[0].data = lessThanSixDays
   chart.config.data.datasets[1].data = moreThanSixDays
   chart.update()
@@ -113,137 +112,25 @@ async function getContractsByWarningsData(chart,year){
   var twoWarnings=[0,0,0,0,0,0,0,0,0,0,0,0]
   var threeOrMoreWarnings=[0,0,0,0,0,0,0,0,0,0,0,0]
 
-  // console.log(contractList)
   contractList.forEach(contract=>{
     const contractYearFilter =Number(contract.fechaRecepcion.split('/')[2])
-    // console.log(contract.mainStatus)
-    if (contract.mainStatus==="Closed"&&contractYearFilter===year){
-      Array.prototype.push.apply(totalhistoricos,contract.historico)
-    }})
-
-    if (filterType === 'Users'){
-      userFilter=document.getElementById('user-filter')
-      var totalDays = [0,0,0,0,0,0,0,0,0,0,0,0]
-      var countResponses = [0,0,0,0,0,0,0,0,0,0,0,0]
-      // userFilter = userElement.name+" "+userElement.surname
-      indexEspacio = userFilter.indexOf(" ",0)
-      let persona = userFilter.slice(0,indexEspacio+2)+"."
-      // console.log(persona)
-
-      var indexLastEmail = 0
-      if (persona==='All U.'){
-        
-      } else {
-
+    if (contract.mainStatus==="Closed"&&contractYearFilter===Number(year)){
+      const numAvisos = getNumAvisos(contract.historico)
+      // console.log(numAvisos)
+      const monthNumber = Number(contract.fechaStatusWon.split('/')[1])
+      if (numAvisos ===0){
+        oneWarning[monthNumber] = oneWarning[monthNumber]+1
+      } else if (numAvisos===1){
+        twoWarnings[monthNumber] = twoWarnings[monthNumber]+1
+      } else if (numAvisos >= 2){
+        threeOrMoreWarnings[monthNumber] = threeOrMoreWarnings[monthNumber]+1
       }
-      // historicoRelevante.forEach((accion,index)=>{
-      //   if (accion.icono!=='mail-unread-outline' && accion.persona.includes(persona)){
-      //     let days = getDaysBetweenDates(historicoRelevante[indexLastEmail].fecha,accion.fecha)
-      //     // console.log("Last Email:", historicoRelevante[indexLastEmail].accion,historicoRelevante[indexLastEmail].fecha, "-->", accion.fecha,"(",days,")",accion.accion)
-
-      //     const monthNumber = Number(accion.fecha.split('/')[1])
-      //     totalDays[monthNumber-1]=totalDays[monthNumber-1]+days
-      //     countResponses[monthNumber-1]=countResponses[monthNumber-1]+1
-          
-      //   } else if (accion.icono==='mail-unread-outline'){
-      //     indexLastEmail = index
-      //   }
-      // })
-
-      // result = totalDays.map((element,index)=>{
-      //   if (countResponses[index] === 0){
-      //     return null
-      //   } else {
-      //     return Math.round(100*element/countResponses[index])/100
-      //   }
-      // })
-
-      // // console.log("Acumulated Response Days:",totalDays)
-      // // console.log("Responses Counted:",countResponses)
-      // // console.log("Data to Plot:",result)
-
-      // let randomColor=getRandomColor()
-      // randomColorBorder=randomColor + "1)"
-      // randomColorFill=randomColor + "0.2)"
-      // newDataSet = {
-      //   label:userFilter,
-      //   data:result,
-      //   pointStyle:'circle',
-      //   pointRadius:'2',
-      //   borderWidth:1,
-      //   fill:false,
-      //   borderColor:randomColorBorder,
-      //   pointBackgroundColor:randomColorBorder,
-      //   backgroundColor:randomColorFill,
-      // }
-
-      // chart.config.data.datasets.push(newDataSet)
-
-    }else if (filterType==="Departments"){
-      // departments.forEach((departmentsElement,departmentIndex)=>{
-      //   var totalDays = [0,0,0,0,0,0,0,0,0,0,0,0]
-      //   var countResponses = [0,0,0,0,0,0,0,0,0,0,0,0]
-      //   // userFilter = userElement.name+" "+userElement.surname
-      //   // indexEspacio = userFilter.indexOf(" ",0)
-      //   // let persona = userFilter.slice(0,indexEspacio+2)+"."
-      //   // console.log(persona)
-  
-      //   var indexLastEmail = 0
-  
-      //   historicoRelevante.forEach((accion,index)=>{
-      //     if (accion.icono!=='mail-unread-outline' && accion.persona.includes(departmentsElement)){
-      //       let days = getDaysBetweenDates(historicoRelevante[indexLastEmail].fecha,accion.fecha)
-      //       // console.log("Last Email:", historicoRelevante[indexLastEmail].accion,historicoRelevante[indexLastEmail].fecha, "-->", accion.fecha,"(",days,")",accion.accion)
-  
-      //       const monthNumber = Number(accion.fecha.split('/')[1])
-      //       totalDays[monthNumber-1]=totalDays[monthNumber-1]+days
-      //       countResponses[monthNumber-1]=countResponses[monthNumber-1]+1
-            
-      //     } else if (accion.icono==='mail-unread-outline'){
-      //       indexLastEmail = index
-      //     }
-      //   })
-  
-      //   result = totalDays.map((element,index)=>{
-      //     if (countResponses[index] === 0){
-      //       return null
-      //     } else {
-      //       return Math.round(100*element/countResponses[index])/100
-      //     }
-      //   })
-  
-      //   // console.log("Acumulated Response Days:",totalDays)
-      //   // console.log("Responses Counted:",countResponses)
-      //   // console.log("Data to Plot:",result)
-  
-      //   let departmentName=''
-      //   switch(departmentsElement) {
-      //     case 'Oper': departmentName="Operaciones"; departmentColor='#06535C';break;
-      //     case 'Come': departmentName="Comercial"; departmentColor='#67E7F5';break;
-      //     case 'Riesg': departmentName="Control de Riesgos"; departmentColor='#0FC7DB';break;
-      //     case 'PRL': departmentName="PRL"; departmentColor='#42595C';break;
-      //     case 'Gener': departmentName="Dirección General"; departmentColor='#0B99A8';break;
-      //     default:
-      //   }
-      //   newDataSet = {
-      //     label:departmentName,
-      //     data:result,
-      //     pointStyle:'circle',
-      //     pointRadius:'2',
-      //     borderWidth:1,
-      //     fill:false,
-      //     borderColor:departmentColor,
-      //     pointBackgroundColor:departmentColor,
-      //     backgroundColor:departmentColor,
-      //   }
-      //   chart.config.data.datasets.push(newDataSet)
-      // })
-    }
+    }})
+    chart.config.data.datasets[0].data = oneWarning
+    chart.config.data.datasets[1].data = twoWarnings
+    chart.config.data.datasets[2].data = threeOrMoreWarnings
     chart.update()
-
 }
-
-
 async function getContractsByAverageResponseDays(chart,filterType,year){
   const contractList =  await getContracts()
   const users=await getUsers()
@@ -397,48 +284,23 @@ var warningsRequired = new Chart(warningsRequiredElement, {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     datasets: [{
       label: '1st Warning',
-      data: [4, 4, 2],
-      backgroundColor: [
-        'rgba(113, 236, 133, 0.2)',
-        'rgba(113, 236, 133, 0.2)',
-        'rgba(113, 236, 133, 0.2)'
-          
-      ],
-      borderColor: [
-        'rgba(113, 236, 133, 1)',
-        'rgba(113, 236, 133, 1)',
-        'rgba(113, 236, 133, 1)',
-      ],
+      data: '',
+      backgroundColor:'rgba(113, 236, 133, 0.2)',
+      borderColor: 'rgba(113, 236, 133, 1)',
       borderWidth: 1
     },
     {
       label: '2nd Warning',
-      data: [2, 1, 2],
-      backgroundColor: [
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(255, 206, 86, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 206, 86, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(255, 206, 86, 1)'
-      ],
+      data: '',
+      backgroundColor: 'rgba(255, 206, 86, 0.2)',
+      borderColor:'rgba(255, 206, 86, 1)',
       borderWidth: 1
     },
     {
       label: '3rd Warning or more',
-      data: [2, 2, 4],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(255, 99, 132, 1)',
-        'rgba(255, 99, 132, 1)',
-      ],
+      data: '',
+      backgroundColor:'rgba(255, 99, 132, 0.2)',
+      borderColor:'rgba(255, 99, 132, 1)',
       borderWidth: 1
     }]
   },
@@ -610,7 +472,7 @@ var averageReceptionWonChart = new Chart(averageReceptionWon, {
         backgroundColor:'rgba(113, 236, 133, 0.2)',
         pointBackgroundColor:'rgba(113, 236, 133, 1)',
         borderColor:'rgba(113, 236, 133, 1)',
-        borderWidth: 1,
+        borderWidth: 2,
         fill:false,
       },
       {
@@ -619,7 +481,7 @@ var averageReceptionWonChart = new Chart(averageReceptionWon, {
         backgroundColor:'rgba(255, 99, 132, 0.2)',
         pointBackgroundColor:'rgba(255, 99, 132, 1)',
         borderColor:'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
+        borderWidth: 2,
         fill:false,
       }]
     },
@@ -653,7 +515,7 @@ var averageReceptionWonChart = new Chart(averageReceptionWon, {
       },
       elements: {
         point:{
-            radius: 2,
+            radius: 4,
         }
       },
     }
@@ -666,8 +528,9 @@ const currentYear = currentDate.getFullYear()
 getAverageDaysFromCreationDateData(averageReceptionWonChart,currentYear)
 getContractsManagedInSixDaysData(sixWarningsChart,currentYear)
 getContractsByAverageResponseDays(averageResponseDaysChart,'Users',currentYear)
+getContractsByWarningsData(warningsRequired,currentYear)
 setYearsToYearFilter()
-setUsersOnSelectFilter()
+// setUsersOnSelectFilter()
 showFilterSelectorOnAverageResponseDaysChart()
 // setCurrentDateInInput()
 // startHidingFilterSelectors()
@@ -744,8 +607,8 @@ function startHidingFilterSelectors(){
 function showFilterSelectorOnAverageResponseDaysChart(select){
   // console.log(select.value)
   if (select===undefined){
-    document.getElementById('user-filter').style.display='block'
-    document.getElementById('department-filter').style.display='none'
+    // document.getElementById('user-filter').style.display='block'
+    // document.getElementById('department-filter').style.display='none'
   } else{
     if (select.value === "Users" || select.value===undefined){
       document.getElementById('user-filter').style.display='block'
@@ -761,7 +624,7 @@ function showFilterSelectorOnAverageResponseDaysChart(select){
 }
 function updateChartContractsByWarningsData(){
   let year = Number(document.getElementById('year-filter').value)
-  getContractsByWarningsData(averageResponseDaysChart,year)
+  getContractsByWarningsData(warningsRequired,year)
 }
 function updateChartContractsByAverageResponseDays(){
   let filterType = document.getElementById('select-filter-ard').value
@@ -811,8 +674,8 @@ function setCurrentDateInInput(){
 }
 function setYearsToYearFilter(){
   const currentDate = new Date()
-  // const currentYear = currentDate.getFullYear()
-  const currentYear = 2025
+  const currentYear = currentDate.getFullYear()
+  // const currentYear = 2025
   const firstYear = 2020
   var select = document.getElementById('year-filter')
   for (i=firstYear;i<=currentYear;i++){
@@ -821,4 +684,26 @@ function setYearsToYearFilter(){
     newOption.text=i
     select.add(newOption)
   }
+}
+function getNumAvisos(historico){
+  // console.log("---- NEW CONTRACT ---")
+  // let indexLastEmail = 0
+  let maxNumAvisos = 0
+  let numAvisos = 0
+  for (i=0;i<historico.length;i++){
+    if (historico[i].icono === "mail-unread-outline" && historico[i].accion!=="Recordatorio Enviado"){
+      // indexLastEmail = i;
+      numAvisos=0;
+    } else {
+      if (historico[i].accion==="Recordatorio Enviado"){
+        numAvisos +=1
+      }
+      if (numAvisos > maxNumAvisos){
+        maxNumAvisos = numAvisos
+      }
+    }
+    // console.log("Nº Av:",numAvisos," | Max Nº Av:",maxNumAvisos," | Hist:",historico[i])
+    
+  }
+  return maxNumAvisos
 }
